cfb5bb4901faafb194f6fba65efd11e8d4a9dcea
NiaGeary-Andrews <74965796+NiaGeary-Andrews@users.noreply.github.com>
2025-02-28 15:10:23 +0000

    Added in floating text which annotates the organs when hovered over

--- Assets/Scripts/ClickableObject.cs
+++ Assets/Scripts/ClickableObject.cs
@@ -1,9 +1,17 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
+
 
 public class ClickableObject : MonoBehaviour
 {
 
     private Outline outline;
+    public string objectName;
+
+    [Header("Prefabs")]
+    public GameObject FloatingTextPrefab;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,6 +32,8 @@ public class ClickableObject : MonoBehaviour
             Debug.Log(this);
             outline.OutlineColor = Color.green;
             outline.enabled = true;
+            //might need a function to hide floating text
+            ShowFloatingText(objectName);
         }
     }
 
@@ -39,5 +49,35 @@ public class ClickableObject : MonoBehaviour
     {
         outline.OutlineColor = Color.red;
         outline.enabled = true;
+    }
+
+    private void ShowFloatingText(string text)
+    {
+        Vector3 screenPosition = Camera.main.WorldToScreenPoint(transform.position + new Vector3(0.1f, 0.1f, 0));
+        //GameObject floatingTextInstance = Instantiate(FloatingTextPrefab, screenPosition, Quaternion.identity, );
+        GameObject floatingTextInstance = Instantiate(FloatingTextPrefab, GameObject.Find("Canvas").transform);
+        floatingTextInstance.GetComponentInChildren<TextMeshProUGUI>().text = text;
+
+        RectTransform rectTransform = floatingTextInstance.GetComponent<RectTransform>();
+        rectTransform.position = screenPosition;
+
+        StartCoroutine(FadeOutAndDestroy(floatingTextInstance, 2.0f));
+    }
+
+    private IEnumerator FadeOutAndDestroy(GameObject floatingTextInstance, float duration)
+    {
+        TextMeshProUGUI tmp = floatingTextInstance.GetComponentInChildren<TextMeshProUGUI>();
+        Color originalColor = tmp.color;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Lerp(1f, 0f, elapsedTime / duration);
+            tmp.color = new Color(originalColor.r, originalColor.g, originalColor.b, alpha);
+            yield return null;
+        }
+
+        Destroy(floatingTextInstance);
     }
 }
